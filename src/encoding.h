22290351592e29604c7223bc59e93e45470cbb31
AnaelleJaffre <Anaelle.Jaffre@ensc.fr>
2025-11-17 17:36:02 +0100

    first draft of encoding of the straight numbers

---
+++ src/encoding.h
@@ -0,0 +1,41 @@
+#ifndef __ENCODING_H__
+#define __ENCODING_H__
+
+// Top left quarter
+#define C1N0 0b0000
+#define C1N1 0b0000
+#define C1N2 0b0001
+
+// Top right quarter
+#define C2N0 0b0010
+#define C2N1 0b0011
+#define C2N2 0b0100
+#define C2N3 0b0101
+#define C2N4 0b0110
+#define C2N5 0b0111
+#define C2N6 0b1000
+#define C2N7 0b1001
+#define C2N8 0b1010
+#define C2N9 0b1011
+
+// Bottom left quarter
+#define C3N0 0b1100
+#define C3N1 0b1101
+#define C3N2 0b1110
+#define C3N3 0b1111
+#define C3N4 0b0000
+#define C3N5 0b0001
+
+// Bottom right quarter
+#define C4N0 0b0010
+#define C4N1 0b0011
+#define C4N2 0b0100
+#define C4N3 0b0101
+#define C4N4 0b0110
+#define C4N5 0b0111
+#define C4N6 0b0010
+#define C4N7 0b0011
+#define C4N8 0b0100
+#define C4N9 0b0101
+
+#endif // __ENCODING_H__
