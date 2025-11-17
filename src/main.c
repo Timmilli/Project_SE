abc8647681a5b59c47fcf3757f30415a60811668
Guenael Roger <guenael.mail@gmail.com>
2025-11-17 16:03:03 +0100

    working on interupts, wiat for 18th nov course

--- src/main.c
+++ src/main.c
@@ -3,26 +3,41 @@
 #include "buffer.h"
 #include "led_com.h"
 #include "uart_com.h"
+#include <avr/interrupt.h>
 #include <avr/io.h>
+#include <string.h>
 #include <util/delay.h>
+
+struct ring_buffer rb;
+uint16_t datastreak = 0b1111100000011111;
+uint8_t nb_interrupt = 0;
+
+ISR(USART_RX_vect) {
+  datastreak = ~datastreak;
+  while (!uart_available())
+    ;
+  uart_send_byte(nb_interrupt);
+  nb_interrupt++;
+  // if (!ring_buffer_is_full(&rb))
+  //   ring_buffer_put(&rb, uart_read_byte());
+}
+
+ISR(USART_UDRE_vect) {
+  if (!ring_buffer_available_bytes(&rb))
+    uart_send_byte(ring_buffer_get(&rb));
+}
 
 int main(void) {
   setup_led_driver_com();
   uart_init(MYUBRR);
-  struct ring_buffer rb;
   ring_buffer_init(&rb);
 
-  uint16_t datastreak = 0;
+  sei(); // activate interrupts
 
   while (1) {
-    pwm(5);
-
     write_datastreak(datastreak);
-    if (datastreak == 0b1111111111111111) {
-      datastreak = 0;
-    } else {
-      datastreak++;
-    }
+
+    pwm(100);
   }
   return 1;
 }
