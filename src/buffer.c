896f21c9212619d9297073d86ab2e4e43c96231e
Guenael Roger <guenael.mail@gmail.com>
2025-11-19 08:16:44 +0100

    struct ring_buffer to ring_buffer_t and new function to process buffer

--- src/buffer.c
+++ src/buffer.c
@@ -4,25 +4,25 @@
 #include <avr/io.h>
 #include <stdio.h>
 
-void ring_buffer_init(struct ring_buffer *rb) {
+void ring_buffer_init(ring_buffer_t *rb) {
   rb->tail = 0;
   rb->head = 0;
   for (uint8_t i = 0; i < RING_BUFFER_SIZE; i++)
     rb->data[i] = 0;
 }
 
-void ring_buffer_put(struct ring_buffer *rb, uint8_t data) {
+void ring_buffer_put(ring_buffer_t *rb, uint8_t data) {
   rb->data[rb->head] = data;
   rb->head = (rb->head + 1) % RING_BUFFER_SIZE;
 }
 
-uint8_t ring_buffer_get(struct ring_buffer *rb) {
+uint8_t ring_buffer_get(ring_buffer_t *rb) {
   uint8_t data = rb->data[rb->tail];
   rb->tail = (rb->tail + 1) % RING_BUFFER_SIZE;
   return data;
 }
 
-uint8_t ring_buffer_available_bytes(struct ring_buffer *rb) {
+uint8_t ring_buffer_available_bytes(ring_buffer_t *rb) {
   if (rb->head >= rb->tail) {
     return rb->head - rb->tail;
   } else {
@@ -30,12 +30,41 @@ uint8_t ring_buffer_available_bytes(struct ring_buffer *rb) {
   }
 }
 
-uint8_t ring_buffer_is_full(struct ring_buffer *rb) {
+uint8_t ring_buffer_is_full(ring_buffer_t *rb) {
   return ((rb->head + 1) % RING_BUFFER_SIZE) == rb->tail;
 }
 
-void ring_buffer_print(struct ring_buffer *rb) {
+void ring_buffer_print(ring_buffer_t *rb) {
   for (uint8_t i = 0; i < RING_BUFFER_SIZE; i++) {
     printf("%c", rb->data[i]);
   }
+}
+
+uint8_t str_cmp(char fstr[3], char sstr[3]) {
+  for (uint8_t i = 0; i < 3; i++) {
+    if (fstr[i] != sstr[i])
+      return 0;
+  }
+  return 1;
+}
+
+uint8_t process_ring_buffer(ring_buffer_t *rb) {
+  // TODO
+  // define transmission format (setHHMMSS)
+  // check if length >= 9
+  // then process
+  // or ff
+  if (ring_buffer_available_bytes(rb) >= 3) {
+    char command[3] = "";
+    for (uint8_t i = 0; i < 3; i++) {
+      command[i] = ring_buffer_get(rb);
+    }
+    for (uint8_t i = 0; i < 6; i++) {
+      ring_buffer_get(rb);
+    }
+
+    if (str_cmp(command, "set"))
+      return SET_HOUR;
+  }
+  return NONE;
 }
--- src/buffer.h
+++ src/buffer.h
@@ -7,31 +7,39 @@
 
 #define RING_BUFFER_SIZE 32
 
-struct ring_buffer {
+typedef enum {
+  NONE = 0,
+  SET_HOUR = 1,
+} process_action_e;
+
+typedef struct {
   uint8_t head;
   uint8_t tail;
   uint8_t data[RING_BUFFER_SIZE];
-};
+} ring_buffer_t;
 
 // Initialize the buffer
 // Also resets the data array
-void ring_buffer_init(struct ring_buffer *rb);
+void ring_buffer_init(ring_buffer_t *rb);
 
 // Adds a byte to the buffer
 // Does not check if buffer full
-void ring_buffer_put(struct ring_buffer *rb, uint8_t data);
+void ring_buffer_put(ring_buffer_t *rb, uint8_t data);
 
 // Get a byte from the buffer
 // Does not check if buffer full
-uint8_t ring_buffer_get(struct ring_buffer *rb);
+uint8_t ring_buffer_get(ring_buffer_t *rb);
 
 // Indicates the number of bytes currently used in the buffer
-uint8_t ring_buffer_available_bytes(struct ring_buffer *rb);
+uint8_t ring_buffer_available_bytes(ring_buffer_t *rb);
 
 // Indicates if the buffer is full
-uint8_t ring_buffer_is_full(struct ring_buffer *rb);
+uint8_t ring_buffer_is_full(ring_buffer_t *rb);
 
 // Prints the buffer content
-void ring_buffer_print(struct ring_buffer *rb);
+void ring_buffer_print(ring_buffer_t *rb);
+
+// Processes the ring buffer into a datastreak
+uint8_t process_ring_buffer(ring_buffer_t *rb);
 
 #endif // __BUFFER_H__
