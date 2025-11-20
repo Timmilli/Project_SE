8458c6794e293cf45cf3bb54a9620c7b822aa947
Guenael Roger <guenael.mail@gmail.com>
2025-11-20 08:27:35 +0100

    removed progmem

--- src/encoding.h
+++ src/encoding.h
@@ -8,47 +8,47 @@
 #define NUMBER_OF_POSITIONS 120
 
 // Top left quarter
-const uint16_t c1n0[NUMBER_OF_POSITIONS] PROGMEM = {0b0101011010100101011,
-                                                    0b0101011010100101011};
+const uint16_t c1n0[NUMBER_OF_POSITIONS] = {0b0101011010100101011,
+                                            0b0101011010100101011};
 #define C1N0 c1n0
 
-const uint16_t c1n1[NUMBER_OF_POSITIONS] PROGMEM = {0b0101011010100101011,
-                                                    0b0101011010100101011};
+const uint16_t c1n1[NUMBER_OF_POSITIONS] = {0b0101011010100101011,
+                                            0b0101011010100101011};
 #define C1N1 c1n1
 
-const uint16_t c1n2[NUMBER_OF_POSITIONS] PROGMEM = {0b0101011010100101011};
+const uint16_t c1n2[NUMBER_OF_POSITIONS] = {0b0101011010100101011};
 #define C1N2 c1n2
 
 // Top right quarter
-#define C2N0 c1n2
-#define C2N1 c1n2
-#define C2N2 c1n2
-#define C2N3 c1n2
-#define C2N4 c1n2
-#define C2N5 c1n2
-#define C2N6 c1n2
-#define C2N7 c1n2
-#define C2N8 c1n2
-#define C2N9 c1n2
+#define C2N0 c1n0
+#define C2N1 c1n0
+#define C2N2 c1n0
+#define C2N3 c1n0
+#define C2N4 c1n0
+#define C2N5 c1n0
+#define C2N6 c1n0
+#define C2N7 c1n0
+#define C2N8 c1n0
+#define C2N9 c1n0
 
 // Bottom left quarter
-#define C3N0 0b1100
-#define C3N1 0b1101
-#define C3N2 0b1110
-#define C3N3 0b1111
-#define C3N4 0b0000
-#define C3N5 0b0001
+#define C3N0 c1n0
+#define C3N1 c1n0
+#define C3N2 c1n0
+#define C3N3 c1n0
+#define C3N4 c1n0
+#define C3N5 c1n0
 
 // Bottom right quarter
-#define C4N0 0b0010
-#define C4N1 0b0011
-#define C4N2 0b0100
-#define C4N3 0b0101
-#define C4N4 0b0110
-#define C4N5 0b0111
-#define C4N6 0b0010
-#define C4N7 0b0011
-#define C4N8 0b0100
-#define C4N9 0b0101
+#define C4N0 c1n0
+#define C4N1 c1n0
+#define C4N2 c1n0
+#define C4N3 c1n0
+#define C4N4 c1n0
+#define C4N5 c1n0
+#define C4N6 c1n0
+#define C4N7 c1n0
+#define C4N8 c1n0
+#define C4N9 c1n0
 
 #endif // __ENCODING_H__
