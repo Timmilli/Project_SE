c1d2e92d95992d80dcc0fa2e94e8724ae4690ad5
Guenael Roger <guenael.mail@gmail.com>
2025-11-21 08:46:20 +0100

    now uses clock_values too

--- src/merge_matrices.c
+++ src/merge_matrices.c
@@ -1,12 +1,15 @@
 #include "constants.h"
 
+#include "clock_module.h"
 #include "merge_matrices.h"
 #include "quarter_selection.h"
 #include <avr/io.h>
 
-void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], uint8_t hours_units,
-                    uint8_t hours_tens, uint8_t minutes_units,
-                    uint8_t minutes_tens) {
+void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], clock_values_t *cv) {
+  uint8_t hours_units = cv->hours % 10;
+  uint8_t hours_tens = cv->hours / 10;
+  uint8_t minutes_units = cv->minutes % 10;
+  uint8_t minutes_tens = cv->minutes / 10;
   const uint16_t *hu_quarter = get_hu_quarter(hours_units);
   const uint16_t *ht_quarter = get_ht_quarter(hours_tens);
   const uint16_t *mu_quarter = get_mu_quarter(minutes_units);
--- src/merge_matrices.h
+++ src/merge_matrices.h
@@ -3,18 +3,14 @@
 
 #include "constants.h"
 
+#include "clock_module.h"
 #include <avr/io.h>
 
 /**
  * Merges the matrices to create the straight clock image
  * @param mat is the final matrix to be filled
- * @param hours_units
- * @param hours_tens forms the hours numbers with hours_units
- * @param minutes_units
- * @param minutes_tens forms the minutes numbers with minutes_units
+ * @param cv the clock values to fill the matrix with
  */
-void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], uint8_t hours_units,
-                    uint8_t hours_tens, uint8_t minutes_units,
-                    uint8_t minutes_tens);
+void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], clock_values_t *cv);
 
 #endif // __MERGE_MATRICES_H__
